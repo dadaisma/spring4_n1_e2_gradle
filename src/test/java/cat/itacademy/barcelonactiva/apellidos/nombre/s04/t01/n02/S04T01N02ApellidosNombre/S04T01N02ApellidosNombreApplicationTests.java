b6bcaf2bac2ae6b101ad3753254392e84331bbbb
dadaisma <infodadaisma@gmail.com>
2024-06-06 21:18:45 +0200

    s4_n1_e2_springboot_gradle

---
+++ src/test/java/cat/itacademy/barcelonactiva/apellidos/nombre/s04/t01/n02/S04T01N02ApellidosNombre/S04T01N02ApellidosNombreApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.apellidos.nombre.s04.t01.n02.S04T01N02ApellidosNombre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02ApellidosNombreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
